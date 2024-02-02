c67613fa35a6e12d118a52c125ccfb317c8b76a8
phil <zhangsphilip@gmail.com>
2024-02-01 20:56:46 -0800

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by 张胖子 on 2024-02-01.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
