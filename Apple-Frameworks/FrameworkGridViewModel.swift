5655a5f9fcad09a3fbf17c5674fc8568f9a66fe8
PhilipZhangs <zhangsphilip@gmail.com>
2024-02-03 15:19:47 -0800

    attempting mvvm structure

---
+++ Apple-Frameworks/FrameworkGridViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  FrameworkGridViewModel.swift
+//  Apple-Frameworks
+//
+//  Created by 张胖子 on 2024-02-03.
+//
+
+import SwiftUI
+
+class FrameworkGridViewModel: ObservableObject {
+    
+    var selectedFramework: Framework? {
+        didSet {
+            isShowingDetailView = true
+        }
+    }
+    
+    @Published var isShowingDetailView = false
+}
