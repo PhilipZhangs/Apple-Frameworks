4b5af873d6969389b7293900eda2f66bfa290287
phil <zhangsphilip@gmail.com>
2024-02-02 18:19:47 -0800

    basics of building apple framework info app

---
+++ Apple-Frameworks/AFButton.swift
@@ -0,0 +1,29 @@
+//
+//  AFButton.swift
+//  Apple-Frameworks
+//
+//  Created by 张胖子 on 2024-02-02.
+//
+
+import SwiftUI
+
+struct AFButton: View {
+    
+    var title: String
+    
+    var body: some View {
+        Text(title)
+            .font(.title2)
+            .fontWeight(.semibold)
+            .frame(width: 200, height: 50)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+        }
+    }
+
+struct AFButton_Previews: PreviewProvider {
+    static var previews: some View {
+        AFButton(title: "Test Title")
+    }
+}
